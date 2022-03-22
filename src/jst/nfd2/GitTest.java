b41fab9ce8f9ba6ac85c2e6bd23f11d057e8cc53
Joachim <joachim.stolle@hotmail.de>
2022-03-22 12:00:33 +0100

    Zweiter commit

--- src/jst/nfd2/GitTest.java
+++ src/jst/nfd2/GitTest.java
@@ -3,5 +3,6 @@ package jst.nfd2;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hallo Git");
+        System.out.println("Dies ist der zweite Commit");
     }
 }
