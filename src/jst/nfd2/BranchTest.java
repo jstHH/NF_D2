3247746417baec9a3b7d5d96d895c58a14cd55d7
Joachim <joachim.stolle@hotmail.de>
2022-03-22 14:14:24 +0100

    neuer Commit

---
+++ src/jst/nfd2/BranchTest.java
@@ -0,0 +1,9 @@
+package jst.nfd2;
+
+import javax.swing.*;
+
+public class BranchTest {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Ein neuer Branch!");
+    }
+}
