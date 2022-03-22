a134f3debbe36a18a92e067583903eec4b9282ce
Joachim <joachim.stolle@hotmail.de>
2022-03-22 12:10:16 +0100

    Dritter commit

--- src/jst/nfd2/GitTest.java
+++ src/jst/nfd2/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hallo Git");
         System.out.println("Dies ist der zweite Commit");
+        System.out.println("Und noch eine Änderung");
     }
 }
